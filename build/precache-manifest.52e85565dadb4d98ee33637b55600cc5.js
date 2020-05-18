18782b58283d4be9206719b26f94939e0ec9e33c
gmd <maoyiluo@gmail.com>
2020-05-18 13:19:38 -0400

    add build

---
+++ build/precache-manifest.52e85565dadb4d98ee33637b55600cc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c6232896a6ddc46607e050556d2d6c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "02fe081221eb274a4fce",
+    "url": "/static/css/main.5f33b63e.chunk.css"
+  },
+  {
+    "revision": "f046217420b039f8d352",
+    "url": "/static/js/2.4cf71c32.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.4cf71c32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02fe081221eb274a4fce",
+    "url": "/static/js/main.da81e6b1.chunk.js"
+  },
+  {
+    "revision": "d2ea5b1a5c30499651ae",
+    "url": "/static/js/runtime-main.2ccad6bb.js"
+  }
+]);
